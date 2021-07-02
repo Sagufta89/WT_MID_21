b453737dd2944b9730e047e9a2a98b3eb276544b
Sagufta89 <85049494+Sagufta89@users.noreply.github.com>
2021-07-02 11:22:08 +0600

    Add files via upload
    php

---
+++ lab-3-Problem1.php
@@ -0,0 +1,18 @@
+<html>
+	<body>
+		Problem-1
+
+		A quick brown
+	</body>	
+</html>
+<?php
+	echo"<b>fox</b>";
+?>
+<html>
+	<body>
+		jumps over the lazy
+	</body>
+</html>
+<?php
+	echo"<b>dog</b>";
+?>
